808aeb2cb088e8cfc6986cb9a4a9fd5b536348b1
Olle Jonsson <olle.jonsson@gmail.com>
2016-11-09 14:06:45 +0100

    Opbeat initializer refactoring

--- addon/initializers/opbeat.js
+++ addon/initializers/opbeat.js
@@ -1,45 +1,45 @@
-
-registerEmberOnError = function(notifyFn) {
-  var originalOnError;
-  originalOnError = Ember.onerror || Ember.K;
-  return Ember.onerror = (function(_this) {
-    return function(err) {
-      originalOnError(err);
-      return notifyFn(err);
-    };
-  })(this);
-};
-
-registerWindowOnError = function(notifyFn) {
-  return window.onerror = function(message, file, line, column, error) {
-    if (message === 'Script error.') {
-      return;
-    }
-    error = error || {
-        error: {
-          message: message,
-          fileName: file,
-          lineNumber: line,
-          columnNumber: column || 0
-        }
-      };
-    return notifyFn(error);
-  };
-};
-
+/* globals _opbeat */
+import Ember from 'ember';
+import ENV from '../config/environment';
 
 export function initialize(/* application */) {
-  var notifyFn, ref, ref1;
-  if ((((ref = ENV.opbeat) != null ? ref.orgId : void 0) != null) && (((ref1 = ENV.opbeat) != null ? ref1.appId : void 0) != null)) {
-    _opbeat('config', ENV.opbeat);
-    notifyFn = function(error) {
-      return _opbeat('captureException', error);
-    };
-    registerWindowOnError(notifyFn);
-    registerEmberOnError(notifyFn);
-  } else {
+  var registerWindowOnError,
+    registerEmberOnError,
+    notifyFn;
+
+  if (!(ENV && ENV.opbeat)) {
     Ember.Logger.warn('Opbeat not configured!', ENV);
+    return;
   }
+
+  registerWindowOnError = function (notifyFn) {
+    return window.onerror = function (message, file, line, column, error) {
+      if (message === 'Script error.') {
+        return;
+      }
+      notifyFn(error || new Error(message, file, line));
+    };
+  };
+
+  registerEmberOnError = function (notifyFn) {
+    var originalOnError;
+    originalOnError = Ember.onerror || Ember.K;
+    return Ember.onerror = (function () {
+      return function (err) {
+        originalOnError(err);
+        return notifyFn(err);
+      };
+    })(this);
+  };
+
+  _opbeat('config', ENV.opbeat);
+
+  notifyFn = function(error) {
+    return _opbeat('captureException', error);
+  };
+
+  registerWindowOnError(notifyFn);
+  registerEmberOnError(notifyFn);
 }
 
 export default {
