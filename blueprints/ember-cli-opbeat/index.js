c2907412cf8ee5e12ce59bff1252e3db04b7e175
Donald Wasserman <djwasserman@gmail.com>
2017-07-24 07:19:26 -0400

    stopped auto removing bower, cleaned up broken code

--- blueprints/ember-cli-opbeat/index.js
+++ blueprints/ember-cli-opbeat/index.js
@@ -1,34 +1,17 @@
-/*jshint node:true*/
-const rsvp = require('rsvp'),
-      fs = require('fs');
+/*eslint node:true*/
 module.exports = {
   description: 'Adds opbeat-js library',
+  init() {
+    this._super.init && this._super.init.apply(this, arguments);
+
+    var bowerDeps = this.project.bowerDependencies();
+
+    if (bowerDeps['pouchdb']) {this.ui.writeWarnLine('Please remove `opbeat-js` from `bower.json`. Bower is deprecated and only the NPM package is needed.');}
+  },
 
   normalizeEntityName() {}, // no-op since we're just adding dependencies
 
   afterInstall() {
-    return this.removePackageFromBowerJSON('opbeat-js')
-      .then(this.addPackageToProject('opbeat-js'))
-  },
-
-  removePackageFromBowerJSON(dependency) {
-    return new rsvp.Promise(function(resolve, reject) {
-      try {
-        let bowerJSONPath = 'bower.json';
-        let bowerJSON = fs.readJsonSync(bowerJSONPath);
-
-        delete bowerJSON.dependencies[dependency];
-
-        fs.writeJsonSync(bowerJSONPath, bowerJSON);
-
-        resolve();
-      } catch(error) {
-        // resolve if bower.json doesn't exist
-        if (error.code == 'ENOENT') {
-          resolve();
-        }
-        reject(error);
-      }
-  });
-},
+    this.addPackageToProject('opbeat-js');
+  }
 };
--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 /* jshint node: true */
 'use strict';
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: 'ember-cli-opbeat',
 
   included() {
     this._super.included.apply(this, arguments);
-    this.import('vendor/moment.js');
-    this.import('vendor/shims/moment.js');
+    this.import('vendor/opbeat.js');
   },
 
   treeForVendor(vendorTree) {
-    var momentTree = new Funnel(path.join(this.project.root, 'node_modules', 'opbeat-js'), {
+    var opbeatTree = new Funnel(path.join(this.project.root, 'node_modules', 'opbeat-js'), {
       files: ['opbeat.js'],
     });
 
-    return new MergeTrees([vendorTree, momentTree]);
+    return new MergeTrees([vendorTree, opbeatTree]);
   },
 };
