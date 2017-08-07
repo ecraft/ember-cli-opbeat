b29f213e2e29873e8471842f98139afed4b8f95f
Donald Wasserman <djwasserman@gmail.com>
2017-08-07 14:42:22 -0400

    fixed copy paste error

--- blueprints/ember-cli-opbeat/index.js
+++ blueprints/ember-cli-opbeat/index.js
@@ -4,9 +4,11 @@ module.exports = {
   init() {
     this._super.init && this._super.init.apply(this, arguments);
 
-    var bowerDeps = this.project.bowerDependencies();
+    let bowerDeps = this.project.bowerDependencies();
 
-    if (bowerDeps['pouchdb']) {this.ui.writeWarnLine('Please remove `opbeat-js` from `bower.json`. Bower is deprecated and only the NPM package is needed.');}
+    if (bowerDeps['opbeat-js']) {
+      this.ui.writeWarnLine('Please remove `opbeat-js` from `bower.json`. Bower is deprecated and only the NPM package is needed.');
+    }
   },
 
   normalizeEntityName() {}, // no-op since we're just adding dependencies
